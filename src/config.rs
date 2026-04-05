2176260408fc845586292079343a047022b532b5
ihua-rgb <ihua@apps.ntvs.ntpc.edu.tw>
2026-04-05 16:09:41 +0800

    更新服務器

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["124.218.220.206"];
+pub const RS_PUB_KEY: &str = "+emCTMIA2l5rhKOQaj9zMLYB+3GV3EpkL0XaWtrXqmc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
